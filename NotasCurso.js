b731da482852f9645378c470ea1483e1ccdf7d7d
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2024-01-17 16:20:55 -0300

    01-Por qué usar typescript?

---
+++ NotasCurso.js
@@ -0,0 +1,6 @@
+/** 01-Por qué usar typescript?
+ * *Es muy usado por la comunidad y muy recomendado además
+ * *Su tipado ayuda a prevenir errores
+ * *Muchas tecnologías están escritas en TS 
+ * *Puede prevenir hasta un 15% de bugs
+ */
