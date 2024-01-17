abe3404251ea7d779afe6c72b72d79d3492d47a3
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2024-01-17 16:35:26 -0300

    02-JS vs TS

--- NotasCurso.js
+++ NotasCurso.js
@@ -4,3 +4,7 @@
  * *Muchas tecnologías están escritas en TS 
  * *Puede prevenir hasta un 15% de bugs
  */
+/** 02-TypeScript vs. JavaScript
+ * *En JS te das cuenta de que tienes un recién cuando ejecutas el códico
+ * *TS es js pero con tipos. Eso incluye todas las nuevas versiones de js y el js actual y además tiene un análisis estático del código
+ */
