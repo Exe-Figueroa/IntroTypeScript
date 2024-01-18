43fb4344645f07f20b854dd08887e50d2192483c
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2024-01-18 11:27:15 -0300

    11-Strings

--- src/06-strings.ts
+++ src/06-strings.ts
@@ -1,1 +1,16 @@
- 
+(() => {
+  let productTitle = 'Hi i´m a product'; //Type string inferido
+  productTitle = 'title changed';
+  let productDescription = 'Description of product';
+  console.log({
+    productTitle,
+    productDescription
+  });
+  const summary = `
+  title: ${productTitle},
+  description: ${productDescription}
+  `;
+  console.log({ summary });
+
+
+})();
